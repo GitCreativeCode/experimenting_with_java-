ed1122f95c1e8f38f0a4d81028850d96dc086756
Ahmed Shaaban <ahmedsh29906@gmail.com>
2024-05-22 12:34:54 -0400

    Updated matrix

--- src/MatrixOperations.java
+++ src/MatrixOperations.java
@@ -4,7 +4,7 @@ public class MatrixOperations {
         int[][] array2 = {{13,12,3, 4, 1}, {3,12,3,4,1}}; 
         int[][] result = new int[array1.length][array1[0].length];
 
-        int[][] array3 = {{-2, -4}, {-6, 3}};
+        int[][] array3 = {{-2, -6}, {-4, 3}, {5, 0}, {4, -6}};
         int[][] array4 = {{2, -2, 2}, {-2, 0, -3}};
         int[][] result2 = new int[array3.length][array4[0].length];
 
@@ -22,7 +22,7 @@ public class MatrixOperations {
 
        for (int i = 0; i < array3.length; i++) {
         for (int j = 0; j < array4[0].length; j++) {
-            for (int k = 0; k < array3[0].length; k++) { // Corrected here
+            for (int k = 0; k < array3[0].length; k++) { 
                 result2[i][j] += array3[i][k] * array4[k][j];
             }
         }
