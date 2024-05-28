113c0616c9cf8e17f88210b44d4ad4a47de132de
Ahmed Shaaban <ahmedsh29906@gmail.com>
2024-05-28 17:58:08 -0400

    added comments

--- src/The2DArray.java
+++ src/The2DArray.java
@@ -8,22 +8,18 @@ public class The2DArray {
         int[][] twoDArray1 = {{13, 123, 33, 12}, {11, 21, 31, 41}, {3, 11, 24, 12}};
         int[][] twoDArray2 = {{32, 21, 43, 11}, {23, 56, 21, 10}, {1, 5, 2, 7}};
 
+        //Pritns the elemnts of arr1 and arr2
         for (int i=0; i<bothArrays.length; i++) {
             System.out.println();
             for(int j=0; j<bothArrays[i].length; j++) {
                 System.out.print(bothArrays[i][j]+" ");
             }
         }
+        //adding array
         System.out.println("");
         for (int k=0; k<arr1.length; k++) {
                 int addingArrays = arr1[k] + arr2[k];
                 System.out.print(addingArrays + " ");
-            }
-            
-        System.out.println("");
-        for (int k=0; k<arr1.length; k++) {
-                int muliplyingArrays = arr1[k] * arr2[k];
-                System.out.print(muliplyingArrays + " ");
             }
 
         System.out.println("This is the 2D multiplication Array:" );
@@ -33,7 +29,7 @@ public class The2DArray {
             System.out.print(result+ " ");
             }
         }
-    
+        //printing the adding array
         System.out.println("This is the 2D Adding Array:" );
         for(int l=0; l<twoDArray1.length; l++){
             for (int i=0; i < twoDArray1[l].length ; i++){
